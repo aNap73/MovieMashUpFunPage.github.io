aab57371a3629c1491dc73f7e030b8299852a923
aNap73 <napolitano.anthony@yahoo.com>
2018-05-29 11:06:08 -0400

    cors

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-
+var cors = require('cors');
 
 var PORT = process.env.PORT || 3000;
 
@@ -20,6 +20,9 @@ var router = require("./controller/GifRoute.js");
 
 
 app.use(router);
+
+
+app.use(cors());
 
 app.listen(PORT, function() {
   console.log("App now listening at localhost:" + PORT);
